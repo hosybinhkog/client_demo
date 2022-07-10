3d152ae366ee9527a1ddbb85d424668afd4f8035
Hồ Sỹ Bình <62041703+hosybinhkog@users.noreply.github.com>
2022-07-11 00:06:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import routes from './routes';
+import { DefaultLayout } from './components';
+import store from '~/redux/store';
+import { loadUser } from '~/redux/actions/user.action';
+
+import UserRoute from './routes/UserRoute';
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {routes.private.map((route, index) => {
+          let Layout = DefaultLayout;
+
+          if (route.layout === null) {
+            Layout = React.Fragment;
+          } else if (route.layout) {
+            Layout = route.layout;
+          }
+
+          const Element = route.component;
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <UserRoute>
+                  <Layout title={route.title}>
+                    <Element />
+                  </Layout>
+                </UserRoute>
+              }
+            />
+          );
+        })}
+        {/* Public route position is END ==> * 404 * */}
+        {routes.publicRoute.map((route, index) => {
+          let Layout = DefaultLayout;
+
+          if (route.layout === null) {
+            Layout = React.Fragment;
+          } else if (route.layout) {
+            Layout = route.layout;
+          }
+
+          const Element = route.component;
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout title={route.title}>
+                  <Element />
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
